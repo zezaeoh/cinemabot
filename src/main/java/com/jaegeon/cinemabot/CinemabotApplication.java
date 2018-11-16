e0a14f8240906e889217aaf3d7dae972ca4a4a63
JeaGeon <resistangce@gmail.com>
2018-11-16 14:26:49 +0900

    Initial commit

---
+++ src/main/java/com/jaegeon/cinemabot/CinemabotApplication.java
@@ -0,0 +1,12 @@
+package com.jaegeon.cinemabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemabotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemabotApplication.class, args);
+    }
+}
